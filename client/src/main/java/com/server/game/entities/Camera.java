4e238f803d3b67c4e3ba91c773192cc976d480f7
Aivars <aivars.sazanovics@gmail.com>
2016-04-03 21:26:14 +0300

    Smooth textures

--- client/src/main/java/com/server/game/entities/Camera.java
+++ client/src/main/java/com/server/game/entities/Camera.java
@@ -1,34 +1,48 @@
 package com.server.game.entities;
 
+import com.server.game.toolbox.Maths;
+import org.lwjgl.Sys;
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
 public class Camera {
 
-    private Vector3f position = new Vector3f(0,5,0);
-    private float pitch = 10;
-    private float yaw ;
-    private float roll;
+    private float speed = 1f;
+    private Vector3f position = new Vector3f(0,5,0); //50
+    private float pitch = 10; // 40
+    private float yaw = 0;
+    private float roll = 0;
 
     public Camera(){}
 
     public void move(){
-        if(Keyboard.isKeyDown(Keyboard.KEY_W)){
-            position.z-=0.2f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_UP)){
+            position.z-=speed;
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_D)){
-            position.x+=0.2f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)){
+            position.z+=speed;
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_A)){
-            position.x-=0.2f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)){
+            position.x+=speed;
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)){
-            position.y+=0.2f;
+        if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)){
+            position.x-=speed;
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_S)){
-            position.y-=0.2f;
+
+        int zoom = Mouse.getDWheel();
+        if (zoom != 0){
+            setZoom(zoom);
         }
-        if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)){
-            position.y-=0.2f;
+    }
+
+    public void setZoom(int zoom){
+        float formattedZoom = zoom*0.01f;
+        if(zoom > 0 && pitch < 90){
+            pitch += formattedZoom;
+            position.setY(position.getY()+formattedZoom);
+        }else if(zoom < 0 && pitch > 0){
+            pitch += formattedZoom;
+            position.setY(position.getY()+formattedZoom);
         }
     }
 
