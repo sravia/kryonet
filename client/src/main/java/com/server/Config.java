d52b0bfb8b291a9bc53feb7aa7ae0d7f9fd48fb2
Aivars Sazanovics <aivars.sazanovics@accenture.com>
2016-04-11 08:47:28 +0300

    Added windows libs

--- client/src/main/java/com/server/Config.java
+++ client/src/main/java/com/server/Config.java
@@ -3,8 +3,8 @@ package com.server;
 public class Config {
 
     public static String HOST = "localhost";
-    public static String RESOURCE_PATH = "client/src/main/resources/";
-    public static String GAME_PATH = "client/src/main/java/com/server/game/";
+    public static String RESOURCE_PATH = System.getProperty("user.dir") + "/src/main/resources/";
+    public static String GAME_PATH = System.getProperty("user.dir") + "/src/main/java/com/server/game/";
     public static int UDP_PORT = 27960;
     public static int TCP_PORT = 27960;
 }
