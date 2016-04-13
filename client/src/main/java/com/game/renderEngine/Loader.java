0e77c1112e723ae68f8f8148f71c9b73c31b7288
Aivars Sazanovics <aivars.sazanovics@accenture.com>
2016-04-13 14:55:13 +0300

    Anisotropic

--- client/src/main/java/com/game/renderEngine/Loader.java
+++ client/src/main/java/com/game/renderEngine/Loader.java
@@ -99,6 +99,12 @@ public class Loader {
             GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER,
                     GL11.GL_LINEAR_MIPMAP_LINEAR);
             GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, 0f);
+            if(GLContext.getCapabilities().GL_EXT_texture_filter_anisotropic){
+                float amount = Math.min(4f,GL11.glGetFloat(EXTTextureFilterAnisotropic.GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT));
+                GL11.glTexParameterf(GL11.GL_TEXTURE_2D,EXTTextureFilterAnisotropic.GL_TEXTURE_MAX_ANISOTROPY_EXT,amount);
+            }else{
+                System.out.println("GL_EXT_texture_filter_anisotropic not supported");
+            }
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println("Tried to load texture " + fileName + ".png , didn't work");
